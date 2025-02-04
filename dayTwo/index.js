7b92e3b2359e99bd74368d6a350c7d6c6691d7da
ianDougherty0706 <idougherty0706@gmail.com>
2025-02-03 21:20:25 -0500

    completed day 3 on my first try

--- dayTwo/index.js
+++ dayTwo/index.js
@@ -1,45 +1,43 @@
-const { log } = require("console");
 const fs = require("fs");
 const path = require("path");
 
 let validCount = 0;
 
-function findSteps(array, length){
-  let direction = array[0] - array[1] < 0;
-  for (let i = 0; i < length; i++){
-    let step = array[i] - array[i + 1];
-    let check = step < 0;
-    // console.log(`${direction}, ${check}`)
-    if ((direction === check) && ((Math.abs(step) <= 3) && (Math.abs(step) >= 1))){
-      validCount++;
-      return true;
-    } else {
+function printArray(array) {
+  array.forEach((element) => {
+    process.stdout.write(`${element} `);
+  });
+  process.stdout.write("\n");
+}
+
+function findSteps(array, length) {
+  printArray(array);
+  const direction = array[0] - array[1] < 0;
+  for (let i = 0; i < length; i++) {
+    const difference = array[i] - array[i + 1];
+    const directionCheck = difference < 0;
+
+    if ((difference >= 3 || difference <= 1) && (direction != directionCheck)){
       return false;
     }
-
   }
+  return true;
 }
 
 let data;
 try {
-    data = fs.readFileSync(path.join(__dirname, "input.txt"), 'utf8');
+  data = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
 } catch (err) {
-    console.error('Error reading the file:', err);
+  console.error("Error reading the file:", err);
 }
 
-data = data.split('\n');
-dataLength = data.length;
+data = data.split("\n");
+data = data.map((sequence) => sequence.split(" "));
+let dataLength = data.length;
 
-let parsedData = [];
-for (let i = 0; i < dataLength; i++){
-  parsedData.push(data[i].split(" "));
-}
-
-let parsedDataLength = parsedData.length;
-let returnValue;
-for (let j = 0; j < parsedDataLength; j++){
-  returnValue = findSteps(parsedData[j], parsedData[j].length);
-  console.log(`${parsedData[j]}\t${returnValue}`)
+for (let i = 0; i < dataLength; i++) {
+  let dataToPass = data[i].map((number) => parseInt(number));
+  findSteps(dataToPass, dataToPass.length)? validCount++ : console.log("Invalid");
 }
 
 console.log(validCount);
